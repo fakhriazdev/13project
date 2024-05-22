26874f5261bb65f47650e6dbf7e0c10d62464112
FakhriAz Dev <fakhriazdev@gmail.com>
2024-05-22 14:15:31 +0700

    [FIX] FloatingNavbar to Low

--- src/components/navbar/FloatingNavbar.tsx
+++ src/components/navbar/FloatingNavbar.tsx
@@ -68,7 +68,7 @@ const FloatingNav = ({navItems, className,}: {
                     duration: 0.2,
                 }}
                 className={cn(
-                    "md:hidden flex max-w-fit fixed bottom-3 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+                    "md:hidden flex max-w-fit fixed bottom-5 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
                     className
                 )}
             >
